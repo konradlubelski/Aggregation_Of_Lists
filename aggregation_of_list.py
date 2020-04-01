6ffcf062109b38f3fd347bc3b8c9da5b352b6106
konradlubelski <62030308+konradlubelski@users.noreply.github.com>
2020-04-01 13:03:18 +0200

    last commit

--- aggregation_of_list.py
+++ aggregation_of_list.py
@@ -1,12 +1,13 @@
-
 import string
 import random
 
 def generate_list():
+
     list_of_letters=list(string.ascii_lowercase)
     number_of_letter = 26
     main_list = []
-    while number_of_letter>0:
+    
+    while number_of_letter > 0:
         a = random.randint(4, 7)
         number_of_letter -= a
         main_list.append(list_of_letters[0:a])
